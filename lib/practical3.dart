71cb89bc8dfebbbff61de835dc2abc90761f9b4c
ujas3279 <ujas6520@gmail.com>
2020-01-03 10:11:01 +0530

    day1

--- lib/practical3.dart
+++ lib/practical3.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:demo/drawer.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
 class Practical3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar( title:Text('Practical3'),),
       drawer: new DrawerOnly(),
-      body: Center(child: Text('hello'),),
+      body: Center(child: ListView(
+        children: <Widget>[
+          RaisedButton(
+            child:Text('toast') ,
+            onPressed: showtoast,
+            )
+        ],
+      ),)
     );
+  }
+  void showtoast(){
+    Timer.periodic(Duration(seconds: 10), (timer)  {
+       Fluttertoast.showToast(
+                        msg: 'hello this is toast message',
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIos: 3,
+                        backgroundColor: Colors.blue,
+                        textColor: Colors.white,
+                        fontSize: 16.0
+                      );
+});
   }
 }
